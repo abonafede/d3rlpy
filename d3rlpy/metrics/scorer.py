02ffdafaae447deb605f9b5ae27956a7e6770c76
Andrew Bonafede <abonafede5@gmail.com>
2023-03-22 20:46:05 -0400

    Second Try

--- d3rlpy/metrics/scorer.py
+++ d3rlpy/metrics/scorer.py
@@ -579,17 +579,14 @@ def dynamics_prediction_variance_scorer(
 
 def true_q_scorer(algo: AlgoProtocol, episodes: List[Episode]) -> float:
     """
-    Implement True_Q_Scorer
+    Implement True_Q_Scorer.
     """
     for episode in episodes:
         for batch in _make_batches(episode, WINDOW_SIZE, algo.n_frames):
-            # estimate values for next observations
             next_actions = algo.predict([batch.next_observations[0]])
-            next_values = algo.predict_value(
-                [batch.next_observations[0]], next_actions
-            )
+            next_values = algo.predict_value([batch.next_observations[0]], next_actions)
             mask = (1.0 - np.asarray(batch.terminals)).reshape(-1)
-            rewards = np.asarray(batch.next_rewards).reshape(-1)
+            rewards = np.asarray(batch.rewards).reshape(-1)
             if algo.reward_scaler:
                 rewards = algo.reward_scaler.transform_numpy(rewards)
             y = rewards + algo.gamma * cast(np.ndarray, next_values) * mask
